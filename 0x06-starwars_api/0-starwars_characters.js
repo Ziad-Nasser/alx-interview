72c6558734fffd1549beb6d21c9b8e76c1d8ebfe
Ziad-Nasser <ziadnasser177@gmail.com>
2024-11-15 03:25:31 +0200

    interview

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 
 const request = require('request');
-// prettier-ignore
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, body) {
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
-// prettier-ignore
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-  request(actors[x], function (err, body) {
+  request(actors[x], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
     exactOrder(actors, x + 1);

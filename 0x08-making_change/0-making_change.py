96b088a9eaff846b38ab8da5f78c17654389e731
Ziad-Nasser <ziadnasser177@gmail.com>
2024-11-29 02:40:03 +0200

    edit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""Implement Making Change Algorithm"""
+
+"""Implement Making Change function"""
 
 
 def makeChange(coins, total):
-    """ Generate changes needed to reach total"""
+    """Make Change function
+    Args:
+        coins: a list of the values of the coins
+        total: the total value of coins needed
+   RETURN: the fewest number of coins needed to make up the given value total
+    
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    check, steps = 0, 0
-    coins.sort(reverse=True)
-
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        while check < total:
-            check += coin
-            steps += 1
-
-        if check == total:
-            return steps
-
-        check -= coin
-        steps -= 1
-
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
     return -1
